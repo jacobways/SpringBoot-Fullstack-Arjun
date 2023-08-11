85912ed43ccd117a6b4de4e88ebc3ba220bfb2a8
jacob <jacobkjy0501@gmail.com>
2023-08-11 13:37:26 +0900

    파일 재생성

---
+++ student-system/src/main/java/jacob/studentsystem/service/StudentServiceImpl.java
@@ -0,0 +1,18 @@
+package jacob.studentsystem.service;
+
+import jacob.studentsystem.model.Student;
+import jacob.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+}
